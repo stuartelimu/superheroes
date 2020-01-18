5be7f46eefd38339fa1b193cf0701f8027dceefd
ElimuStuart <stuartelimu@gmail.com>
2020-01-18 13:40:52 +0300

    add search feature

--- hero/urls.py
+++ hero/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import HeroView, SingleHeroView
 
 urlpatterns = [
-    path('heroes', HeroView.as_view(), name='hero_list'),
+    path('heroes/', HeroView.as_view(), name='hero_list'),
     path('heroes/<int:pk>/', SingleHeroView.as_view(), name='single_hero'),
 ]
--- hero/views.py
+++ hero/views.py
@@ -1,10 +1,13 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import filters
 
 from .models import Hero
 from .serializers import HeroSerializer
 
 
 class HeroView(ListCreateAPIView):
+    search_fields = ['name']
+    filter_backends = (filters.SearchFilter,)
     queryset = Hero.objects.all()
     serializer_class = HeroSerializer
 
